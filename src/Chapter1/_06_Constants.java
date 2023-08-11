09c29795e1fbacf92e400010d9e2e329d1df24b3
Shinjeonghun <powerpress0809@naver.com>
2023-08-11 11:08:37 +0900

    first Commit

---
+++ src/Chapter1/_06_Constants.java
@@ -0,0 +1,14 @@
+package Chapter1;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호 빨리
+//        KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "1996-08-09"; // 생년 월일
+        System.out.println(DATE_OF_BIRTH);
+
+    }
+}
